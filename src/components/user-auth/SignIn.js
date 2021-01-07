9d1f2b6c1b9f15460c74c5edc0c5d332db35959a
buffolander <brdu1976@gmail.com>
2021-01-07 16:18:21 -0300

    implemented submit on enter, sign-in failure message

--- src/components/user-auth/SignIn.js
+++ src/components/user-auth/SignIn.js
@@ -16,6 +16,7 @@ import {
 
 import {
   Button,
+  FormControl,
   Grid,
   Paper,
   TextField,
@@ -29,6 +30,7 @@ const SignInComponent = () => {
   const history = useHistory()
   const dispatch = useDispatch()
   const fbState = useSelector(state => state.firebase)
+  const authState = useSelector(state => state.auth)
   const [credentials, setCredentials] = useState({
     email: null,
     password: null,
@@ -41,32 +43,45 @@ const SignInComponent = () => {
     [e.target.id]: e.target.value,
   })
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(signIn(credentials))
+  }
+
   return (
     <Grid container item direction="row" justify="center">
       <Paper className={classes.paper}>
         <h1 className={classes.h1}>Login</h1>
-        <TextField id="email"
-          className={classes.textField}
-          size="small"
-          required
-          label="Email"
-          fullWidth={true}
-          onChange={handleChange}
-          variant="outlined" />
-        <TextField id="password"
-          className={classes.textField}
-          size="small"
-          required
-          label="Password"
-          fullWidth={true}
-          type="password"
-          onChange={handleChange}
-          variant="outlined" />
-        <Button color="primary"
-          variant="contained"
-          disableElevation
-          fullWidth={true}
-          onClick={() => dispatch(signIn(credentials))}>Login</Button>
+        <form onSubmit={handleSubmit}
+          pre>
+          <TextField id="email"
+            className={classes.textField}
+            size="small"
+            required
+            label="Email"
+            fullWidth={true}
+            onChange={handleChange}
+            variant="outlined" />
+          <TextField id="password"
+            className={classes.textField}
+            size="small"
+            required
+            label="Password"
+            fullWidth={true}
+            type="password"
+            onChange={handleChange}
+            on
+            variant="outlined" />
+          <Button color="primary"
+            type="submit"
+            variant="contained"
+            disableElevation
+            fullWidth={true}>Login</Button>
+        </form>
+        <div style={{ marginTop: '0.5em', color: 'red' }}>{authState.authError
+          ? <span>invalid email or password</span>
+          : <span>&nbsp;</span>}
+        </div>
       </Paper>
     </Grid>
   )
