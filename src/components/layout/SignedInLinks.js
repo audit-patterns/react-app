ab0674d3c103e47033296754f2b2a1c6f3b6ee3d
buffolander <brdu1976@gmail.com>
2021-01-18 17:17:08 -0500

    implemented session reset

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,27 +1,33 @@
 import React from 'react'
 
+import { useDispatch } from 'react-redux'
+
+import { useHistory } from 'react-router-dom'
+
+import { signOut } from '../../store/actions/auth-actions'
+import { resetSession } from '../../store/actions/session-actions'
+
 import {
   Link,
 } from 'react-router-dom'
-
-import {
-  useDispatch,
-} from 'react-redux'
-
-import {
-  signOut,
-} from '../../store/actions/auth-actions'
 
 import {
   Grid,
 } from '@material-ui/core'
 
 const SignedInLinks = () => {
+  const history = useHistory()
   const dispatch = useDispatch()
+
+  const handleReset = () => {
+    dispatch(resetSession())
+    history.push('/file-upload')
+  }
+
   return (
     <Grid item container xs={10} spacing={1} direction="row" justify="flex-end" alignItems="center">
       <Grid item>
-        <Link to="/file-upload">Start over</Link>
+        <Link onClick={handleReset}>Start over</Link>
       </Grid>
       <Grid item>
         <Link to="#" onClick={() => dispatch(signOut())}>Sign out</Link>
